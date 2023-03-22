408c14efebc0f8d51829220022466a76028dc83c
vincenttommi <vincenttommi@gmail.com>
2023-03-22 08:20:49 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,17 @@
+// Define the divide function expression
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Define the square arrow function
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Define the add arrow function
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  module.exports = { divide, square, add };
+  
